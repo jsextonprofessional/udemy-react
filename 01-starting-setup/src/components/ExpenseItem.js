d4d53eefc929e23557b3c8ff3863b0454d173218
Jacob Sexton <jsextonprofessional@protonmail.com>
2022-08-23 14:59:16 -0500

    Coded along throuhg s3e25.

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h1>220823</h1>
+      <div>
+        <h2>Car Insurance</h2>
+        <p>$294.99</p>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
