43b0ae89200c88262972b8ee079e3c5e1da0e33c
Jacob Sexton <jsextonprofessional@protonmail.com>
2022-08-23 11:31:10 -0500

    Finished Udemy React course summary. Coded along through e487.

--- react-course-meetup/src/pages/Favorites.js
+++ react-course-meetup/src/pages/Favorites.js
@@ -1,8 +1,24 @@
+import { useContext } from 'react';
+
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 function FavoritesPage() {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You have no favorites yet. Add some?</p>
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />
+  }
+
   return (
-    <div>
-      <h1>Favorites</h1>
-    </div>
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
   );
 }
 
--- react-course-meetup/src/store/favorites-context.js
+++ react-course-meetup/src/store/favorites-context.js
@@ -19,7 +19,7 @@ export function FavoritesContextProvider(props) {
 
 	function removeFavoriteHandler(meetupId) {
 		setUserFavorites(prevUserFavorites => {
-			return prevUserFavorites.filter(meetup => meetup.id !== meetup.id);
+			return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
 		});
 	}
 
