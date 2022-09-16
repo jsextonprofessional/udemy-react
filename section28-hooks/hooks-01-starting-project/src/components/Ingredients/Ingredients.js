b1bfe3f138847f719ac095cc7a4d83be841e2171
Jacob Sexton <jsextonprofessional@protonmail.com>
2022-09-16 16:41:33 -0500

    Coded along through s28e439. Added delete functionality.

--- section28-hooks/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ section28-hooks/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -49,7 +49,16 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = ingredientId => {
-    setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
+    fetch(
+      `https://react-hooks-update-16663-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, 
+      {
+        method: "DELETE",
+      }
+    ).then(response => {
+      setUserIngredients(prevIngredients =>
+        prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+      );  
+    });
   };
 
   return (
